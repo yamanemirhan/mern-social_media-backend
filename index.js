f8ab04451cee6a19823d2fc07e3b6787339bf28d
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-09-27 21:02:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000,
+    origin: "http://localhost:3000",
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Origin",
