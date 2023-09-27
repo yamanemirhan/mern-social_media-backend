aab91733b767e1880505f3aca8818c3476147700
Emirhan Yaman <90368997+yamanemirhan@users.noreply.github.com>
2023-09-27 21:23:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-socialmedia-app.netlify.app",
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Origin",
